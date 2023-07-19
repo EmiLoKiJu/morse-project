5da29404695d8c6b94fb39992b0526c1e4e466ff
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-07-19 11:44:57 -0700

    Add function
    Co-authored-by mrcbq <marcobaquero@gmail.com>

--- morse.rb
+++ morse.rb
@@ -1,1 +1,38 @@
-# Nothing here yet
+Morse_equals = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def morse_to_text(morsetext)
+    words = morsetext.split('   ')
+    def change_characters
+        
+    characters.each_with_index { |char, i| characters[i] = Morse_equals[char] || '' }
+    # print characters
+end
+
+morse_to_text('-- -.--   -. .- -- .')
